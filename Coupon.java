bca73d42325577adfdbdf56d54bd8601e830446c
Satya <89769507+SatyaRy@users.noreply.github.com>
2025-06-12 20:44:50 +0700

    updated

--- Coupon.java
+++ Coupon.java
@@ -6,7 +6,6 @@ public class Coupon{
     double discount_percentage;
     // Constructors
     public Coupon() {}
-
     public Coupon(String couponID, int coupon_code,String created_date, String expired_date,double discount_percentage) {
         this.couponID = couponID;
         this.coupon_code = coupon_code;
@@ -14,8 +13,6 @@ public class Coupon{
         this.expired_date = expired_date;
         this.discount_percentage = discount_percentage;
     }
-
-    // Getters and Setters
 
  
 }
